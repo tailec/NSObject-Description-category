10ba32cbfd7dbd847176a1cee6d78e49b6ef4294
tailec <pawel@tailec.com>
2015-01-18 21:06:59 +0000

    Initial commit

---
+++ NSObject+Description.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+Description.h
+//  cli
+//
+//  Created by pardnoj on 18/01/2015.
+//  Copyright (c) 2015 pardnoj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Description)
+
+- (NSString*)PRDdescription;
+
+@end
---
+++ NSObject+Description.m
@@ -0,0 +1,33 @@
+//
+//  NSObject+Description.m
+//  cli
+//
+//  Created by pardnoj on 18/01/2015.
+//  Copyright (c) 2015 pardnoj. All rights reserved.
+//
+
+#import "NSObject+Description.h"
+#import "objc/runtime.h"
+
+@implementation NSObject (Description)
+
+- (NSString*)PRDdescription {
+    NSMutableDictionary *dict = [NSMutableDictionary new];
+    unsigned int count;
+    objc_property_t *properties = class_copyPropertyList([self class], &count);
+    
+    for (int i = 0; i < count; i++) {
+        const char *property = property_getName(properties[i]);
+        NSString *propertyString = [NSString stringWithCString:property encoding:[NSString defaultCStringEncoding]];
+        id obj = [self valueForKey:propertyString];
+        [dict setValue:obj forKey:propertyString];
+    }
+    
+    free(properties);
+    return [NSString stringWithFormat:@"<%@ %p %@>",
+            [self class],
+            self,
+            dict];
+}
+
+@end
